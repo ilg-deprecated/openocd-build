29b80da41b822b9994d474f07ebb2106c5170b0d
Liviu Ionescu <ilg@livius.net>
2017-11-10 11:12:12 +0200

    build.sh: update OPENOCD_GIT_COMMIT for 0.10.0-5

--- scripts/build.sh
+++ scripts/build.sh
@@ -286,7 +286,7 @@ OPENOCD_GIT_URL="${OPENOCD_GIT_URL:-https://github.com/gnu-mcu-eclipse/openocd.g
 # OPENOCD_GIT_BRANCH="${OPENOCD_GIT_BRANCH:-gnu-mcu-eclipse}"
 OPENOCD_GIT_BRANCH="${OPENOCD_GIT_BRANCH:-gnu-mcu-eclipse-dev}"
 # OPENOCD_GIT_COMMIT="HEAD"
-OPENOCD_GIT_COMMIT="6f21a8e40e1d159310d38283efae52b54d98a539"
+OPENOCD_GIT_COMMIT="055a70f66f8c27e52798197e11505688b994a241"
 
 # Since some of the original URLs are occasionaly unavailable,
 # the archives were re-published in a dedicated GitHub project:
